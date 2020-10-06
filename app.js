2bc5ef1f81aff64bc1801077ad14e487ed1819b7
maxtasy <maxtasy88@web.de>
2020-10-06 15:58:07 +0200

    Finish Project

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const expressHandlebars = require("express-handlebars");
+const methodOverride = require("method-override");
 const passport = require("passport");
 const expressSession = require("express-session");
 const MongoStore = require("connect-mongo")(expressSession);
@@ -26,13 +27,23 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Method Override
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === "object" && "_method" in req.body) {
+    // Look in urlencoded POST bodies and delete it
+    let method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
+
 // Logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
 // Handlebars Helpers
-const { formatDate, truncate, stripTags, editIcon } = require("./helpers/hbs");
+const { formatDate, truncate, stripTags, editIcon, select } = require("./helpers/hbs");
 
 // Handlebars
 app.engine(".hbs", expressHandlebars({
@@ -40,7 +51,8 @@ app.engine(".hbs", expressHandlebars({
     formatDate,
     truncate,
     stripTags,
-    editIcon
+    editIcon,
+    select
   },
   extname: ".hbs",
   defaultLayout: "main"
--- routes/stories.js
+++ routes/stories.js
@@ -13,7 +13,7 @@ router.get("/add", ensureAuth, (req, res) => {
 // @route POST /stories
 router.post("/", ensureAuth, async (req, res) => {
   try {
-    req.body.user = req.user.id
+    req.body.user = req.user.id;
     await Story.create(req.body);
     res.redirect("/dashboard");
   } catch (err) {
@@ -34,6 +34,109 @@ router.get("/", ensureAuth, async (req, res) => {
     res.render("stories/index", {
       stories: stories
     });
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
+});
+
+// @desc  Show A Story
+// @route GET /stories/:id
+router.get("/:id", ensureAuth, async (req, res) => {
+  try {
+    const story = await Story.findById(req.params.id)
+      .populate("user")
+      .lean();
+
+    res.render("stories/show", {
+      story: story
+    });
+  } catch (err) {
+    console.error(err);
+    res.render("error/404");
+  }
+});
+
+// @desc  Show All Stories of User
+// @route GET /stories/user/:id
+router.get("/user/:id", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({
+      user: req.params.id,
+      status: "public"
+    })
+      .populate("user")
+      .sort({ createdAt: "desc" })
+      .lean();
+
+    res.render("stories/index", {
+      stories: stories
+    });
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
+});
+
+// @desc  Edit Story Page
+// @route GET /stories/edit/:id
+router.get("/edit/:id", ensureAuth, async (req, res) => {
+  try {
+    const story = await Story.findOne({ _id: req.params.id }).lean();
+
+    if (!story) {
+      res.render("error/404");
+    } else if (story.user != req.user.id) {
+      res.redirect("/stories");
+    } else {
+      res.render("stories/edit", {
+        story: story
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
+});
+
+// @desc  Process Edit Form
+// @route PUT /stories/:id
+router.put("/:id", ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).lean();
+
+    if (!story) {
+      res.render("error/404"); 
+    } else if (story.user != req.user.id) {
+      res.redirect("/stories");
+    } else {
+      story = await Story.findOneAndUpdate(
+        { _id: req.params.id }, 
+        req.body,
+        { new: true, runValidators: true }
+      );
+      res.redirect("/dashboard");
+    }
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
+});
+
+// @desc  Delete Story
+// @route DELETE /stories/:id
+router.delete("/:id", ensureAuth, async (req, res) => {
+  try {
+    const story = await Story.findOne({ _id: req.params.id }).lean();
+
+    if (!story) {
+      res.render("error/404");
+    } else if (story.user != req.user.id) {
+      res.redirect("/dashboard");
+    } else {
+      await Story.remove({ _id: req.params.id });
+      res.redirect("/dashboard");
+    }
   } catch (err) {
     console.error(err);
     res.render("error/500");
